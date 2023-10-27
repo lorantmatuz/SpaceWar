f4f8e13264540c3ce1d7d9607b1e1e79aed3bed9
Matuz <matuzlorant20@gmail.com>
2023-10-28 00:02:39 +0200

    Extension for SpaceObjects to move

--- src/main/java/hu/elte/inf/szofttech2023/team3/spacewar/space/Space.java
+++ src/main/java/hu/elte/inf/szofttech2023/team3/spacewar/space/Space.java
@@ -7,7 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This class stores the board for the game and serves as the basis of the
+ * This class implements the space class with a singleton pattern. This stores
+ * the board for the game and serves as the basis of the
  * {@code GenerateSpace} and {@code ShortestPath} classes.
  */
 public class Space {
@@ -51,5 +52,26 @@ public class Space {
     protected void setSpaceObject(Point p, SpaceObject object) {
         objects.add(object);
         isSpaceObject[p.x][p.y] = true;
+    }
+
+    /**
+     * Attempts to move the given object to the gives point in the Space
+     * @param object The SpaceObject to move
+     * @param to the Point in the Space to move the object
+     * @return true if the movement was successful
+     */
+    public boolean moveObject(SpaceObject object, Point to) {
+        if(isSpaceObject[to.x][to.y]) {
+            return false;
+        }
+        final var index = objects.indexOf(object);
+        if(index == -1) {
+            return false;
+        }
+        final var innerObject = objects.get(index);
+        isSpaceObject[innerObject.x][innerObject.y] = false;
+        isSpaceObject[to.x][to.y] = true;
+        innerObject.replace(to.x,to.y);
+        return true;
     }
 }
--- src/main/java/hu/elte/inf/szofttech2023/team3/spacewar/space/objects/SpaceObject.java
+++ src/main/java/hu/elte/inf/szofttech2023/team3/spacewar/space/objects/SpaceObject.java
@@ -2,10 +2,14 @@ package hu.elte.inf.szofttech2023.team3.spacewar.space.objects;
 
 import java.awt.Point;
 
-public abstract class SpaceObject {
-    Point coordinate ;
+public abstract class SpaceObject extends Point {
 
     public SpaceObject(int x, int y) {
-        coordinate = new Point(x,y);
+        super(x,y);
+    }
+
+    public void replace(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
