8a39c39ed2b3c3f0e79ca560f773803454afbaf2
matuzl20 <matuzlorant20@gmail.com>
2023-11-15 23:13:28 +0100

    Renamed accumulating cost

--- src/main/java/hu/elte/inf/szofttech2023/team3/spacewar/model/space/ArcOfPath.java
+++ src/main/java/hu/elte/inf/szofttech2023/team3/spacewar/model/space/ArcOfPath.java
@@ -2,4 +2,4 @@ package hu.elte.inf.szofttech2023.team3.spacewar.model.space;
 
 import java.awt.*;
 
-public record ArcOfPath(Point node, double cost) { }
+public record ArcOfPath(Point node, double accumulatingCost) { }
