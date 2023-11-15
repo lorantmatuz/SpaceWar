7adcd9565d0ca1a2bf2d057c054a3ea0554d8c5a
matuzl20 <matuzlorant20@gmail.com>
2023-11-15 11:22:59 +0100

    Add basic functions to Player

--- src/main/java/hu/elte/inf/szofttech2023/team3/spacewar/model/game/Player.java
+++ src/main/java/hu/elte/inf/szofttech2023/team3/spacewar/model/game/Player.java
@@ -4,15 +4,35 @@ import hu.elte.inf.szofttech2023.team3.spacewar.model.space.objects.Planet;
 import hu.elte.inf.szofttech2023.team3.spacewar.model.space.ships.Fleet;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Player
 {
-    ArrayList<Planet> planets;
-    ArrayList<Fleet> fleets;
+    private final String name;
+    private final List<Planet> planets = new ArrayList<>();
+    private final List<Fleet> fleets = new ArrayList<>();
 
-    public Player(){}
+    public Player(String name) {
+        this.name = name;
+    }
 
-    public void init()
-    {
+    public void addPlanet(Planet planet) {
+        planets.add(planet);
+    }
+
+    public void addFleet(Fleet fleet) {
+        fleets.add(fleet);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Planet> getPlanets() {
+        return planets;
+    }
+
+    public List<Fleet> getFleets() {
+        return fleets;
     }
 }
