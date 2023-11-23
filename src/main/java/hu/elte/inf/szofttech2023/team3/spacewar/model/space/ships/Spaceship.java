22ab51e0e6556fd0db217eee49f5a7895d24d581
matuzl20 <matuzlorant20@gmail.com>
2023-11-23 17:09:37 +0100

    Add Spaceship.java with basic functions

--- src/main/java/hu/elte/inf/szofttech2023/team3/spacewar/model/space/ships/Spaceship.java
+++ src/main/java/hu/elte/inf/szofttech2023/team3/spacewar/model/space/ships/Spaceship.java
@@ -1,31 +1,11 @@
 package hu.elte.inf.szofttech2023.team3.spacewar.model.space.ships;
 
-public enum Spaceship {
-    MOTHER_SHIP(3,4,3,5,4,4,0),
-    HUNTER      (3,2,4,2,2,2,1),
-    CRUISER     (4,3,5,3,3,3,1),
-    FRIGATE     (4,4,3,4,4,4,2),
-    COLONY      (2,3,3,4,3,3,2),
-    SUPPLIER    (1,2,3,2,1,2,2);
 
-    public final int offensiveForce;
-    public final int protectiveForce;
-    public final int speed;
-    public final int metalCost;
-    public final int consumption;
-    public final int minLevelToBuild;
-    private int healthPoint;
+public class Spaceship {
+    public final SpaceshipEnum spaceship;
 
-    Spaceship(int offensiveForce, int protectiveForce,
-              int speed, int healthPoint,
-              int metalCost, int consumption, int minLevelToBuild) {
-        this.offensiveForce = offensiveForce;
-        this.protectiveForce = protectiveForce;
-        this.speed = speed;
-        this.healthPoint = healthPoint;
-        this.metalCost = metalCost;
-        this.consumption = consumption;
-        this.minLevelToBuild = minLevelToBuild;
+    public Spaceship(SpaceshipEnum spaceship) {
+        this.spaceship = spaceship;
     }
 
     /**
@@ -33,7 +13,7 @@ public enum Spaceship {
      * @return true if the defender is destroyed after the attack
      */
     public boolean attack(Spaceship defender) {
-        defender.healthPoint -= offensiveForce;
+        defender.spaceship.decreaseHealthPoint(spaceship.offensiveForce);
         return defender.isDestroyed();
     }
 
@@ -49,6 +29,6 @@ public enum Spaceship {
      * @return true if the Spaceship is destroyed
      */
     public boolean isDestroyed() {
-        return healthPoint < 0;
+        return spaceship.getHealthPoint() < 0;
     }
 }
