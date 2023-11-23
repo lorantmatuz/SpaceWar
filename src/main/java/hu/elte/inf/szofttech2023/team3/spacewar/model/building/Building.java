e1e2407965532a1f60a52f564bdb8eec4c022735
matuzl20 <matuzlorant20@gmail.com>
2023-11-23 17:10:34 +0100

    Remove upgrade from building

--- src/main/java/hu/elte/inf/szofttech2023/team3/spacewar/model/building/Building.java
+++ src/main/java/hu/elte/inf/szofttech2023/team3/spacewar/model/building/Building.java
@@ -9,46 +9,23 @@ import hu.elte.inf.szofttech2023.team3.spacewar.model.space.objects.Planet;
 public abstract class Building {
     protected Planet planet;
     protected int level = 1;
-    private boolean isUnderConstruction = false;
-    private int timeLeft = 0;
 
     public Building(Planet planet) {
         this.planet = planet;
     }
 
-    public abstract void upgrade() ;
-
-    public void scheduleUpgrade() {
-        timeLeft = getDurationOfConstruction();
-        isUnderConstruction = true;
-    }
-
-    public boolean checkUpgrade() {
-        if(!isUnderConstruction) {
-            return false;
-        }
-        if(--timeLeft == 0) {
-            upgrade();
-            isUnderConstruction = false;
-            return true;
-        }
-        return false;
-    }
+    public abstract void upgrade();
 
     public int getLevel() {
         return level;
     }
 
-    public int getDurationOfConstruction() {
+    public int getDurationOfUpgrade() {
         return level;
     }
 
-    public boolean isUnderConstruction() {
-        return isUnderConstruction;
-    }
 
-    // TODO: modify the upgrade cost of level function
-    public int upgradeCostOfLevel() {
+    public int getUpgradeCostOfLevel() {
         return (int)Math.pow(2,level);
     }
 
